2d127474b8fb39116df2dcfb59c797316b25e705
Dan Lipsitt <danlipsitt@gmail.com>
2015-09-24 14:58:12 -0700

    Printer app.

--- printers/models.py
+++ printers/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Printer(models.Model):
+    hostname = models.CharField(max_length=255)
+    port = models.IntegerField(default=5000)
+
+    def __str__(self):
+        return self.hostname
