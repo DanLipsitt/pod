8f975ed6fb00efc61a059f273350a069cb23030d
Dan Lipsitt <danlipsitt@gmail.com>
2015-09-28 17:34:45 -0700

    Reset initial migration for Jobs.

--- jobs/migrations/0001_initial.py
+++ jobs/migrations/0001_initial.py
@@ -11,6 +11,12 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
+            name='Material',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+            ],
+        ),
+        migrations.CreateModel(
             name='PrintAssembly',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
@@ -26,12 +32,47 @@ class Migration(migrations.Migration):
             name='PrintPart',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('quantity_needed', models.IntegerField()),
+                ('assembly', models.ForeignKey(to='jobs.PrintAssembly')),
+                ('material', models.ForeignKey(to='jobs.Material')),
             ],
         ),
         migrations.CreateModel(
-            name='SlicedModel',
+            name='SlicedModelFile',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
             ],
+        ),
+        migrations.CreateModel(
+            name='SlicedModelRev',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('rev', models.IntegerField()),
+                ('part', models.ForeignKey(to='jobs.PrintPart')),
+                ('sliced_model', models.ForeignKey(to='jobs.SlicedModelFile')),
+            ],
+            options={
+                'get_latest_by': 'rev',
+            },
+        ),
+        migrations.CreateModel(
+            name='SourceModelFile',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+            ],
+        ),
+        migrations.AddField(
+            model_name='slicedmodelfile',
+            name='source_model',
+            field=models.ForeignKey(to='jobs.SourceModelFile'),
+        ),
+        migrations.AddField(
+            model_name='printassembly',
+            name='job',
+            field=models.ForeignKey(to='jobs.PrintJob'),
+        ),
+        migrations.AlterUniqueTogether(
+            name='slicedmodelrev',
+            unique_together=set([('part', 'rev')]),
         ),
     ]
