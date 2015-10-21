1c9dc0530970f7d5e87a5dafbcefd3c93266eda2
Dan Lipsitt <danlipsitt@gmail.com>
2015-10-20 17:35:58 -0700

    More sophisticated dummy upload using grequests and httpbin.
    Upload a small StringIO buffer to httpbin running on localhost.

--- files/models.py
+++ files/models.py
@@ -8,9 +8,23 @@ class PrintFile(models.Model):
 
 
 def distribute_file(f, filename, printers):
-    import time
+    import grequests
+    from io import StringIO
+    from requests_toolbelt import MultipartEncoderMonitor
 
+    url_template = "http://localhost:8081/post"
+
+    def make_progress_callback(url):
+        def progress(monitor):
+            print(url, monitor.bytes_read)
+        return progress
+
+    reqs = []
     for printer in printers:
-        print("starting {}".format(printer))
-        time.sleep(1)
-        print("finished {}".format(printer))
+        url = url_template.format()
+        callback = make_progress_callback(url)
+        data = {'gcode': ('filename', StringIO('dummy data'))}
+        monitor = MultipartEncoderMonitor.from_fields(data, callback)
+        headers = {'Content-Type': monitor.content_type}
+        reqs.append(grequests.post(url, data=monitor, headers=headers))
+    return grequests.map(reqs)
