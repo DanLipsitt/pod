ce74ff1ed6d4a529ea64e7e3e8984c3b7f598102
Dan Lipsitt <danlipsitt@gmail.com>
2015-11-01 12:05:02 -0800

    Some webpack config cleanup.

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,17 @@
 var path = require('path');
 
 module.exports = {
-    entry: "./client/js/main.jsx",
-    output: {
-        path: path.join(__dirname, "build/js"),
-        filename: "bundle.js"
-    },
-    module: {
-        loaders: [
-            {test: /\.css$/, loader: "style!css"},
-            {test: /\.js$/,  exclude: /node_modules/, loader: "babel-loader"},
-            {test: /\.jsx$/, exclude: /node_modules/, loader: "babel-loader"},
-        ]
-    }
+  entry: './client/main.jsx',
+  output: {
+    path: './build',
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {test: /\.css$/, loader: 'style!css'},
+      {test: /\.scss$/, loader: 'style!css!sass'},
+      {test: /\.js$/,  exclude: /node_modules/, loader: 'babel-loader'},
+      {test: /\.jsx$/, exclude: /node_modules/, loader: 'babel-loader'},
+    ],
+  },
 };
