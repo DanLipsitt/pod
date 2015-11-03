14e5c7e07bbd49b248dc00ee7ab86d63b9c01f13
Dan Lipsitt <danlipsitt@gmail.com>
2015-11-03 01:06:02 -0800

    Fix errors in PrinterCard component.

--- client/components/printer-card.jsx
+++ client/components/printer-card.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import {Row, Col, Card, Button, Glyph} from 'elemental';
 
-export class PrinterCard extends Card {
+export class PrinterCard extends React.Component {
   render() {
     return (
       <Card>
       <h3>{this.props.name}</h3>
         <Row>
-          <Col sm="2/3" gutter="5"><Card><Glyph icon="device-camera-video"></Glyph></Card></Col>
-          <Col sm="1/3" gutter="5"><Card>Ready</Card></Col>
+          <Col sm="2/3" gutter={5}><Card><Glyph icon="device-camera-video"></Glyph></Card></Col>
+          <Col sm="1/3" gutter={5}><Card>Ready</Card></Col>
         </Row>
         <Row>No file loaded.</Row>
         <Row>
