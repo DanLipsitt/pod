0c3396cde0281940a38290d337cfe41a28d52a55
Dan Lipsitt <danlipsitt@gmail.com>
2015-11-03 00:46:08 -0800

    Simplify FileList component and add key.

--- client/components/file-list.jsx
+++ client/components/file-list.jsx
@@ -3,21 +3,17 @@ import {Button, Glyph, Table, Alert} from 'elemental';
 import {FileItem} from './file-item';
 
 export function FileList(props) {
-  let content = null;
-  if (!props.data) {
-    content = <Alert type="warning">No files yet...</Alert>;
-  } else {
-    let items = props.data.map(
-      item => <FileItem filename={item} />
-    );
-    content = <ul>{items}</ul>;
-  }
-
   return (
     <div>
       <h2><Glyph icon="file-directory"/> Files</h2>
       <Button><Glyph icon="plus" />Add File</Button>
-      {content}
+      {!props.data ?
+       <Alert type="warning">No files yet...</Alert> :
+       <ul>
+         {props.data.map(item =>
+           <FileItem filename={item} key={item} />
+          )}
+       </ul>}
     </div>
   );
 };
