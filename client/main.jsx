ac2aa3884966210c5a0d8148e3d64df6058fa8e5
Dan Lipsitt <danlipsitt@gmail.com>
2015-11-02 16:37:29 -0800

    PrinterGrid.

--- client/main.jsx
+++ client/main.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Row, Col, Card, Button, Glyph} from 'elemental';
 import './style/style.less';
-import {PrinterCard} from './components/printer-card';
+import {PrinterGrid} from './components/printer-grid';
 
-class App extends  React.Component {
+class App extends React.Component {
   render() {
     return (
       <div>
@@ -15,31 +15,7 @@ class App extends  React.Component {
           <Button><Glyph icon="plus"></Glyph>Add Job</Button>
 </Col>
           <Col sm="3/4">
-            <h2>Printers</h2>
-            <Row>
-              <Col sm='1/3'>
-              <Card>
-                <h3>series1-2000</h3>
-                <Row>
-                  <Col sm="2/3"><Card><Glyph icon="device-camera-video"></Glyph></Card></Col>
-                  <Col sm="1/3"><Card>Ready</Card></Col>
-                </Row>
-                <Row>No file loaded.</Row>
-                <Row>
-                  <Button><Glyph icon="triangle-right"></Glyph></Button>
-                  <Button><Glyph icon="primitive-square"></Glyph></Button>
-                  <Button><Glyph icon="database"></Glyph>Filament</Button>
-                </Row>
-              </Card>
-              </Col>
-              <Col sm='1/3'><PrinterCard name="series1-2000"></PrinterCard></Col>
-              <Col sm='1/3'><Card><h3>series1-2000</h3></Card></Col>
-            </Row>
-            <Row>
-              <Col sm='1/3'><Card><h3>series1-2000</h3></Card></Col>
-              <Col sm='1/3'><Card><h3>series1-2000</h3></Card></Col>
-              <Col sm='1/3'><Card><h3>series1-2000</h3></Card></Col>
-            </Row>
+          <PrinterGrid />
           </Col>
         </Row>
         <footer>Footer</footer>
