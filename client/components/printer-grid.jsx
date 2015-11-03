3044aa184b97bf47943a18a475786db624019d52
Dan Lipsitt <danlipsitt@gmail.com>
2015-11-02 16:57:05 -0800

    Printer grid as stateless arrow funciton.
    https://facebook.github.io/react/docs/reusable-components.html#stateless-functions

--- client/components/printer-grid.jsx
+++ client/components/printer-grid.jsx
@@ -2,22 +2,17 @@ import React from 'react';
 import {Row, Col} from 'elemental';
 import {PrinterCard} from './printer-card';
 
-export class PrinterGrid extends React.Component {
-  render() {
-    return (
-      <div>
-        <h2>Printers</h2>
-        <Row>
-          <Col sm='1/3'><PrinterCard name="series1-2001"></PrinterCard></Col>
-          <Col sm='1/3'><PrinterCard name="series1-2002"></PrinterCard></Col>
-          <Col sm='1/3'><PrinterCard name="series1-2003"></PrinterCard></Col>
-        </Row>
-        <Row>
-          <Col sm='1/3'><PrinterCard name="series1-2004"></PrinterCard></Col>
-          <Col sm='1/3'><PrinterCard name="series1-2005"></PrinterCard></Col>
-          <Col sm='1/3'><PrinterCard name="series1-2006"></PrinterCard></Col>
-        </Row>
-      </div>
-    );
-  };
-};
+export var PrinterGrid = (props) =>
+  <div>
+    <h2>Printers</h2>
+    <Row>
+      <Col sm='1/3'><PrinterCard name="series1-2001"></PrinterCard></Col>
+      <Col sm='1/3'><PrinterCard name="series1-2002"></PrinterCard></Col>
+      <Col sm='1/3'><PrinterCard name="series1-2003"></PrinterCard></Col>
+    </Row>
+    <Row>
+      <Col sm='1/3'><PrinterCard name="series1-2004"></PrinterCard></Col>
+      <Col sm='1/3'><PrinterCard name="series1-2005"></PrinterCard></Col>
+      <Col sm='1/3'><PrinterCard name="series1-2006"></PrinterCard></Col>
+    </Row>
+  </div>;
