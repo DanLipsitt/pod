67ccc3e6b24ad1dd2fc2cc5b52c135048540d186
Dan Lipsitt <danlipsitt@gmail.com>
2015-11-04 15:30:18 -0800

    Mockup of print history.

--- client/main.jsx
+++ client/main.jsx
@@ -13,6 +13,12 @@ class App extends React.Component {
         <Row>
           <Col sm="1/4">
             <FileList data={this.props.files}/>
+            <div>
+              <h2><Glyph icon="history"/> History</h2>
+              <p><b>file5.gcode</b> completed on <b>series1-2003</b> last Wed.</p>
+              <p><b>file1.gcode</b> completed on <b>series1-2001</b> at 2pm yesterday</p>
+              <p><b>file2.gcode</b> cancelled on <b>series1-2003</b> a few seconds ago.</p>
+            </div>
           </Col>
           <Col sm="3/4">
           <PrinterGrid />
