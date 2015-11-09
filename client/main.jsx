fc231bf4c4a5cee117a7ba2209149049dfae045a
Dan Lipsitt <danlipsitt@gmail.com>
2015-11-09 13:32:24 -0800

    Use a history icon present in glyphicons.

--- client/main.jsx
+++ client/main.jsx
@@ -18,7 +18,7 @@ class App extends React.Component {
           <Col sm={3}>
             <FileList data={this.props.files}/>
             <div>
-              <h2><Glyphicon glyph="history"/> History</h2>
+              <h2><Glyphicon glyph="hourglass"/> History</h2>
               <p><b>file5.gcode</b> completed on <b>series1-2003</b> last Wed.</p>
               <p><b>file1.gcode</b> completed on <b>series1-2001</b> at 2pm yesterday</p>
               <p><b>file2.gcode</b> cancelled on <b>series1-2003</b> a few seconds ago.</p>
