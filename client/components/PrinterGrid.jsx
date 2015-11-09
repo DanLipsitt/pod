1d6090984421af6949208c76b9f3c448428646d2
Dan Lipsitt <danlipsitt@gmail.com>
2015-11-09 13:32:00 -0800

    Fix printer grid colum width.

--- client/components/PrinterGrid.jsx
+++ client/components/PrinterGrid.jsx
@@ -7,13 +7,13 @@ export var PrinterGrid = (props) =>
   <div>
     <h2>Printers</h2>
     <Row>
-      <Col sm={3}><PrinterCard name="series1-2001"></PrinterCard></Col>
-      <Col sm={3}><PrinterCard name="series1-2002"></PrinterCard></Col>
-      <Col sm={3}><PrinterCard name="series1-2003"></PrinterCard></Col>
+      <Col sm={4}><PrinterCard name="series1-2001"></PrinterCard></Col>
+      <Col sm={4}><PrinterCard name="series1-2002"></PrinterCard></Col>
+      <Col sm={4}><PrinterCard name="series1-2003"></PrinterCard></Col>
     </Row>
     <Row>
-      <Col sm={3}><PrinterCard name="series1-2004"></PrinterCard></Col>
-      <Col sm={3}><PrinterCard name="series1-2005"></PrinterCard></Col>
-      <Col sm={3}><PrinterCard name="series1-2006"></PrinterCard></Col>
+      <Col sm={4}><PrinterCard name="series1-2004"></PrinterCard></Col>
+      <Col sm={4}><PrinterCard name="series1-2005"></PrinterCard></Col>
+      <Col sm={4}><PrinterCard name="series1-2006"></PrinterCard></Col>
     </Row>
   </div>;
