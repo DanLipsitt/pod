9d2bec3be8b75525d8237ac11f329a2a5d16825e
Dan Lipsitt <danlipsitt@gmail.com>
2015-11-12 17:29:18 -0800

    Move hot model reload transform from babel config to playground config.
    This allows the main app to work.

--- webpack.config.playground.js
+++ webpack.config.playground.js
@@ -9,6 +9,26 @@ module.exports = _.extend(devConfig, {
     'webpack-hot-middleware/client',
     './client/playground/index'
   ],
+  loaders: [{
+    test: /\.jsx?$/,
+    loader: 'babel',
+    include: path.join(__dirname, 'src'),
+    query: {
+      plugins: ['react-transform'],
+      extra: {
+        'react-transform': {
+          transforms: [{
+            transform: 'react-transform-hmr',
+            imports: ['react'],
+            locals: ['module']
+          }, {
+            transform: 'react-transform-catch-errors',
+            imports: ['react', 'redbox-react']
+          }]
+        }
+      }
+    }
+  }],
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin()
