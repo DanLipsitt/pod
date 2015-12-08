9c5ca5bb1c4cb83e407b224f7b2a9cafba021847
Dan Lipsitt <danlipsitt@gmail.com>
2015-12-08 14:41:28 -0800

    Test file upload filename.

--- files/tests.py
+++ files/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.core.urlresolvers import reverse
+from django.core.files.uploadedfile import SimpleUploadedFile
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class TestPrintFileViewSet(APITestCase):
+
+    def test_filename_from_request(self):
+        """
+        The original upload filename should be passed to the serializer.
+        """
+        url = reverse('printfile-list')
+        data = {'file': SimpleUploadedFile('file.gcode', b'...')}
+        resp = self.client.post(url, data)
+        self.assertEqual(resp.data['filename'], data['file'].name)
