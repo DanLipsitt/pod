4b17247d1cad399b00edd24ea5f05b975c191296
Dan Lipsitt <danlipsitt@gmail.com>
2015-12-07 17:02:29 -0800

    Send CSRF token with dropzone file upload.
    https://docs.djangoproject.com/en/1.9/ref/csrf/#ajax
    http://www.dropzonejs.com/#config-headers
    https://github.com/felixrieseberg/React-Dropzone-Component#custom-post-parameters

--- client/components/UploadDropzone.jsx
+++ client/components/UploadDropzone.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import cookie from 'react-cookie';
 import DropzoneComponent from 'react-dropzone-component';
 import 'dropzone/dist/min/dropzone.min.css';
 import 'react-dropzone-component/styles/filepicker.css';
@@ -7,7 +8,10 @@ const UploadDropzone = (props) => {
   let config = {
     postUrl: "/api/files/",
   };
-  return <DropzoneComponent config={config} />
+  let djsConfig = {
+    headers: {"X-CSRFToken": cookie.load('csrftoken')}
+  };
+  return <DropzoneComponent config={config} djsConfig={djsConfig} />
 }
 
 export default UploadDropzone
