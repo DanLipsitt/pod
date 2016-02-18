3202760f8c6f181c3707cbc6a038157498a903b3
Dan Lipsitt <danlipsitt@gmail.com>
2016-02-17 16:18:59 -0800

    Basic setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='pod',
+    version='0.1.0.dev0',
+    packages=find_packages(),
+    include_package_data=True,
+    license='Proprietary',
+    description='Type A Machines Pod Server',
+    long_description=README,
+    url='https://www.typeamachines.com/',
+    author='Type A Machines',
+    author_email='support@typeamachines.com',
+)
