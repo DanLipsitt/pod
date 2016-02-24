33c264ddafca4e8b24f96094f24ff1445cea6c1c
Dan Lipsitt <danlipsitt@gmail.com>
2016-02-23 16:51:14 -0800

    Mockup of PrinterGroupControl.

--- client/components/PrinterGroupControl.jsx
+++ client/components/PrinterGroupControl.jsx
@@ -1,13 +1,31 @@
 import React from 'react';
-import {Panel, ButtonGroup} from 'react-bootstrap';
-import {StartButton} from './StartStopButtons';
+import {Row, Col, Panel, Button, ButtonGroup,
+        ButtonToolbar} from 'react-bootstrap';
+import {StartButton, PauseButton, StopButton,
+        ResumeButton} from './StartStopButtons';
 
 const PrinterGroupControl = () => (
-  <Panel style={{display:'inline-block', marginLeft:'2em'}}>
-    5 selected {' '}
-    <ButtonGroup>
-      <StartButton>3</StartButton>
-    </ButtonGroup>
+  <Panel style={{display:'inline-block', marginLeft:'2em',
+                 marginBottom: '0em'}}>
+    <Col lg={3}>
+      <Row>5 selected</Row>
+      <Row>
+        <ButtonToolbar>
+          <Button bsSize="xsmall">All</Button> {' '}
+          <Button bsSize="xsmall">None</Button>
+        </ButtonToolbar>
+      </Row>
+    </Col>
+    <Col lg={9}>
+      <ButtonToolbar>
+        <ButtonGroup>
+          <StartButton>2</StartButton>
+          <PauseButton>1</PauseButton>
+          <ResumeButton>1</ResumeButton>
+        </ButtonGroup>
+        <StopButton>1</StopButton>
+      </ButtonToolbar>
+    </Col>
   </Panel>
 );
 
