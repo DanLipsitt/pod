e13528670ffa3343c6d3d36b6fff5f7e82c0f137
Dan Lipsitt <danlipsitt@gmail.com>
2016-03-06 17:58:18 -0800

    New .eslintrc.js for eslint 2.x.

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    mocha: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 6,
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+      jsx: true,
+    },
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    indent: ['error', 4],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    indent: ['warn', 2],
+    'comma-dangle': ['warn', 'always-multiline'],
+    'no-console': ['warn'],
+  },
+};
