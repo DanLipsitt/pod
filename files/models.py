2dc64da4eb51a1527616c505f4e168d488e25542
Dan Lipsitt <danlipsitt@gmail.com>
2016-04-22 12:09:22 -0700

    Print logging models.

--- files/models.py
+++ files/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from printers.models import Printer
+from django.contrib.auth.models import User
 
 
 class PrintFile(models.Model):
@@ -8,3 +10,27 @@ class PrintFile(models.Model):
     createdAt = models.DateTimeField(auto_now_add=True)
 
 
+class PrintRun(models.Model):
+
+    printfile = models.ForeignKey(PrintFile)
+    printer = models.ForeignKey(Printer)
+
+    def state(self):
+        """
+        Current state based on most recent event.
+        """
+        pass                              # FIXME
+
+    def duration(self):
+        """
+        Time from current start until pause, end, or now.
+        """
+        pass                              # FIXME
+
+
+class PrintLog(models.Model):
+
+    printrun = models.ForeignKey(PrintRun)
+    server_time = models.DateTimeField()
+    printer_time = models.DateTimeField()
+    event = models.CharField(max_length=16)
