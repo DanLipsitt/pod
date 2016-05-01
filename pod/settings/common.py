3152e9ed041a5a7f3242ea23fec573e1b6af0b13
Dan Lipsitt <danlipsitt@gmail.com>
2016-05-01 03:26:47 -0700

    Move internationalization stuff, including timezones, to common config.

--- pod/settings/common.py
+++ pod/settings/common.py
@@ -32,3 +32,16 @@ DATABASES = {
         'NAME': str(BASE_DIR / 'db/db.sqlite3'),
     }
 }
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.8/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
