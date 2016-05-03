bb7f812729b9ed4d1d52a89f51b0701acf253f28
Dan Lipsitt <danlipsitt@gmail.com>
2016-05-03 16:34:25 -0700

    Wrap long filenames in history list.

--- client/components/PrintLog.jsx
+++ client/components/PrintLog.jsx
@@ -13,10 +13,12 @@ const events = {
 
 export const eventTypes = Object.keys(events);
 
+let style = {overflowWrap: 'break-word'};
+
 var PrintLog = ({event, filename, host, timestamp}) => {
   const time = new Date(timestamp);
   const eventTag = events[event];
-  return <ListGroupItem header={filename}>
+  return <ListGroupItem header={filename} style={style}>
     {eventTag} on <b>{host}</b>
     { } <Time value={time} relative/>
   </ListGroupItem>;
